c91185d6fc22a42e15b3f9842fa68c77b27f83c3
Jiajie Li <78901505+ljjsalt@users.noreply.github.com>
2021-08-03 03:05:58 +0800

    basic function

--- src/components/Piano.vue
+++ src/components/Piano.vue
@@ -1,24 +1,90 @@
 <template>
   <div>
-    <button @click="test">play</button>
+    <button @click="test_play">play</button>
+    <button @click="test_stop">stop</button>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import {piano_preset} from "../assets/instruments/piano"
+import { defineComponent, computed } from "vue";
+import * as Tone from "tone";
+import {useStore} from "vuex"
 
 export default defineComponent({
   name: "piano",
   setup() {
-
-		let test = ()=>{
-		console.log(piano_preset.zones)
-		}
+    const store = useStore()
+    let bindings = computed(() => {
+      return store.state.bindings;
+    }).value
 
     return {
-			test
-		};
+      DEV: true, // dev mode
+      enableBlackKey: false, // black key status
+      lastKeyCode: null, // the last key pressed
+      keyLock: false, // ?
+
+      now: null,
+      synth: new Tone.Synth().toDestination(),
+
+      bindings
+    };
+  },
+  mounted() {
+    this.bindKeyBoradEvent();
+  },
+  methods: {
+    test_play() {
+      const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+      const now = Tone.now();
+      synth.triggerAttack("D4", now);
+      synth.triggerAttack("F4", now + 0.5);
+      synth.triggerAttack("A4", now + 1);
+      synth.triggerAttack("C4", now + 1.5);
+      synth.triggerAttack("E4", now + 2);
+      synth.triggerRelease(["D4", "F4", "A4", "C4", "E4"], now + 4);
+    },
+
+    test_stop() {},
+
+    initInstrument() {},
+    bindKeyBoradEvent() {
+      const ShiftKeyCode = 16;
+      document.addEventListener(
+        "keydown",
+        (e) => {
+          let keyCode = e.keyCode;
+          if (this.DEV) console.log("keydown", keyCode);
+
+          // combination key
+          if (keyCode == ShiftKeyCode) {
+            this.enableBlackKey = true;
+          }
+          if (this.enableBlackKey) keyCode = "b" + keyCode;
+
+          this.playByKey(keyCode);
+          this.lastKeyCode = keyCode;
+        },
+        false
+      );
+
+      document.addEventListener(
+        "keyup",
+        (e) => {
+          console.log("keyup");
+          if (e.keyCode == ShiftKeyCode) {
+            this.enableBlackKey = false;
+          }
+          // $(`.wkey`).removeClass("wkey-active");
+          // $(`.bkey`).removeClass("bkey-active");
+        },
+        false
+      );
+    },
+
+    playByKey(keyCode) {
+      this.synth.triggerAttackRelease(this.bindings[keyCode], "8n");
+    },
   },
 });
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,34 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    bindings: {
+      81: "C5", // Q
+      87: "D5", // W
+      69: "E5", // E
+      82: "F5", // R
+      85: "G5", // R
+      73: "A5", // R
+      79: "B5", // R
+
+      65: "C4", // A
+      83: "D4", // S
+      68: "E4", // D
+      70: "F4", // F
+      74: "G4", // J
+      75: "A4", // K
+      76: "B4", // L
+
+      90: "C3", // Z
+      88: "D3", // X
+      67: "E3", // C
+      86: "F3", // V
+      77: "G3", // M
+      188: "A3", // <
+      190: "B3", // >
+
+      72: "C6", // H
+
+    }
   },
   mutations: {
   },
